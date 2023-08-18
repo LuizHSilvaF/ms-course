b9235e1d7aca29c208458d6f4310e53b4cd0f669
LuizHSilvaF <luiz.henriqueoff10@gmail.com>
2023-08-18 13:00:15 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/example/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/example/hrworker/controllers/WorkerController.java
@@ -3,6 +3,7 @@ package com.example.hrworker.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,6 @@ import com.example.hrworker.repositories.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class WorkerController {
 
-	
 	@Autowired
 	private WorkerRepository repo;
 	
